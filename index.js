eed373bb81534a0fcfdd4830ff7fe9184d38b1cb
Jamie Eastabrook <jame.eastabrook@gmail.com>
2018-03-31 20:58:41 +0100

    Adjusting rules to ensure fat arrow functions require jsdocs.

--- index.js
+++ index.js
@@ -1,99 +1,96 @@
 module.exports = {
-  "parser": "babel-eslint",
-  "env": {
-    "browser": true,
-    "node": true,
-    "jest": true
+  parser: "babel-eslint",
+  env: {
+    browser: true,
+    node: true,
+    jest: true,
   },
-  "extends": [
+  extends: [
     "airbnb",
     "plugin:flowtype/recommended",
     "plugin:css-modules/recommended",
     "prettier",
     "prettier/flowtype",
-    "prettier/react"
+    "prettier/react",
   ],
-  "plugins": ["flowtype", "css-modules", "prettier", "jsdoc", "require-jsdoc"],
-  "rules": {
+  plugins: ["flowtype", "css-modules", "prettier", "jsdoc", "require-jsdoc"],
+  rules: {
     "no-console": [
       "error",
       {
-        "allow": ["warn", "error", "info"]
-      }
+        allow: ["warn", "error", "info"],
+      },
     ],
     "react/jsx-indent": [2, 2],
-    "react/jsx-filename-extension": [
-      "error",
-      { "extensions": [".js", ".jsx"] }
-    ],
+    "react/jsx-filename-extension": ["error", { extensions: [".js", ".jsx"] }],
     "capitalized-comments": [
       "error",
       "always",
       {
-        "ignorePattern": "pragma|ignored",
-        "ignoreInlineComments": true
-      }
+        ignorePattern: "pragma|ignored",
+        ignoreInlineComments: true,
+      },
     ],
     "spaced-comment": [
       "error",
       "always",
       {
-        "line": {
-          "markers": ["/"],
-          "exceptions": ["-", "+"]
+        line: {
+          markers: ["/"],
+          exceptions: ["-", "+"],
         },
-        "block": {
-          "markers": ["!"],
-          "exceptions": ["*"],
-          "balanced": true
-        }
-      }
+        block: {
+          markers: ["!"],
+          exceptions: ["*"],
+          balanced: true,
+        },
+      },
     ],
     "space-before-blocks": ["error", "always"],
-    "import/no-extraneous-dependencies": ["error", { "packageDir": "." }],
+    "import/no-extraneous-dependencies": ["error", { packageDir: "." }],
     "import/extensions": 0,
     "import/no-unresolved": 0,
     "prefer-destructuring": [
       "error",
       {
-        "VariableDeclarator": {
-          "array": false,
-          "object": true
+        VariableDeclarator: {
+          array: false,
+          object: true,
         },
-        "AssignmentExpression": {
-          "array": false,
-          "object": false
-        }
+        AssignmentExpression: {
+          array: false,
+          object: false,
+        },
       },
       {
-        "enforceForRenamedProperties": false
-      }
+        enforceForRenamedProperties: false,
+      },
     ],
     "prettier/prettier": [
       "error",
       {
-        "singleQuote": true,
-        "trailingComma": "all",
-        "bracketSpacing": true
-      }
+        singleQuote: true,
+        trailingComma: "all",
+        bracketSpacing: true,
+      },
     ],
     "jsx-a11y/label-has-for": [
       2,
       {
-        "components": ["Label"],
-        "required": {
-          "every": ["id"]
+        components: ["Label"],
+        required: {
+          every: ["id"],
         },
-        "allowChildren": false
-      }
+        allowChildren: false,
+      },
     ],
     "jsx-a11y/anchor-is-valid": [
       "error",
       {
-        "components": ["Link"],
-        "specialLink": ["to"],
-        "aspects": ["noHref", "invalidHref", "preferButton"]
-      }
+        components: ["Link"],
+        specialLink: ["to"],
+        aspects: ["noHref", "invalidHref", "preferButton"],
+      },
     ],
     "react/prefer-stateless-function": "off",
     "jsdoc/check-param-names": 2,
@@ -109,13 +106,24 @@ module.exports = {
     "jsdoc/require-param-type": 2,
     "jsdoc/require-returns-description": 0,
     "jsdoc/require-returns-type": 2,
-    "require-jsdoc": 2
+    "require-jsdoc": [
+      "error",
+      {
+        require: {
+          FunctionDeclaration: true,
+          MethodDefinition: true,
+          ClassDeclaration: true,
+          ArrowFunctionExpression: true,
+          FunctionExpression: true,
+        },
+      },
+    ],
   },
-  "settings": {
+  settings: {
     "import/resolver": {
-      "node": {
-        "moduleDirectory": ["node_modules", "src"]
-      }
-    }
-  }
+      node: {
+        moduleDirectory: ["node_modules", "src"],
+      },
+    },
+  },
 };
