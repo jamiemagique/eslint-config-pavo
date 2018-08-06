7cb9096b0e686fc12abba68354148ae08cb3d687
Jamie Eastabrook <jame.eastabrook@gmail.com>
2018-08-06 11:17:26 +0100

    feat(updates): updates all packages :seedling:
    Updates all packages to the latest versions.
    
    Removes eslint rules and `find-new-rules` script usage so we can’t have to double up and keep rules in sync in this lib.

--- react.js
+++ react.js
@@ -2,43 +2,9 @@ module.exports = {
   env: {
     browser: true,
   },
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
   extends: ['airbnb', 'prettier', 'prettier/standard', 'prettier/react'],
   plugins: ['react', 'jsx-a11y'],
   rules: {
-    'react/jsx-indent': [2, 2],
     'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
-    'react/button-has-type': 2,
-    'react/forbid-dom-props': 0,
-    'react/destructuring-assignment': 2,
-    'react/prefer-stateless-function': 2,
-    'react/jsx-child-element-spacing': 2,
-    'react/jsx-sort-props': 2,
-    'react/jsx-sort-default-props': 2,
-    'react/no-access-state-in-setstate': 2,
-    'react/jsx-max-depth': [1, { max: 10 }],
-    'react/no-this-in-sfc': 2,
-    'jsx-a11y/label-has-for': [
-      2,
-      {
-        components: ['Label'],
-        required: {
-          every: ['id'],
-        },
-        allowChildren: false,
-      },
-    ],
-    'jsx-a11y/anchor-is-valid': [
-      'error',
-      {
-        components: ['Link'],
-        specialLink: ['to'],
-        aspects: ['noHref', 'invalidHref', 'preferButton'],
-      },
-    ],
   },
 };
